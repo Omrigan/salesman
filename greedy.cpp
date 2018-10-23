81ceaedf68280246b53d020aa28683688578e2b7
NedoL98 <a.nedol@yandex.ru>
2018-10-23 12:13:44 +0300

    Even better greedy structure

--- greedy.cpp
+++ greedy.cpp
@@ -1,5 +1,24 @@
 #include "basic_structs.cpp" //nosubmit
 
+struct GreedyManager {
+    GreedyManager() = delete;
+
+    GreedyManager(const Assignment* task_, int day_ = 1)
+        : task(task_)
+        , day(day_) {
+        solution.task = task;
+        airport = task->start;
+        visited.assign(task->N, false);
+        visited[airport->zone] = true;
+    }
+    
+    Airport* airport;
+    int day;
+    vector<bool> visited;
+    Solution solution;
+    const Assignment* task;
+};
+
 // returns false iff there is no more suitable edges
 bool iterate_to_next_edge(const vector<bool>& visited, const vector<const Edge*>& edges, int& ind) {
     ind = min(ind + 1, static_cast<int>(edges.size()));
@@ -10,20 +29,20 @@ bool iterate_to_next_edge(const vector<bool>& visited, const vector<const Edge*>
     return false;
 }
 
-int get_suitable_edges(const vector<bool>& visited, const Airport* airport, int day, int max_edge_ind) {
+int get_suitable_edges(GreedyManager* mngr) {
     int suitable_edges_cnt = 0;
 
     int ind = -1;
-    while (iterate_to_next_edge(visited, airport->edges_from_by_day[day], ind)) {
+    while (iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[mngr->day], ind)) {
         ++suitable_edges_cnt;
-        if (suitable_edges_cnt == max_edge_ind) {
+        if (suitable_edges_cnt == mngr->task->max_edge_index) {
             return suitable_edges_cnt;
         }
     }
     ind = -1;
-    while (iterate_to_next_edge(visited, airport->edges_from_by_day[0], ind)) {
+    while (iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[0], ind)) {
         ++suitable_edges_cnt;
-        if (suitable_edges_cnt == max_edge_ind) {
+        if (suitable_edges_cnt == mngr->task->max_edge_index) {
             return suitable_edges_cnt;
         }
     }
@@ -31,34 +50,35 @@ int get_suitable_edges(const vector<bool>& visited, const Airport* airport, int 
 }
 
 // really nasty
-Edge const* get_suitable_edge(const vector<bool>& visited, const Airport* airport, int day, int ind) {
+Edge const* get_suitable_edge(GreedyManager* mngr, int ind) {
     int cur_ind_day = -1;
     int cur_ind_zero = -1;
 
     int edges_found = 0;
 
-    iterate_to_next_edge(visited, airport->edges_from_by_day[day], cur_ind_day);
-    iterate_to_next_edge(visited, airport->edges_from_by_day[0], cur_ind_zero);
+    iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[mngr->day], cur_ind_day);
+    iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[0], cur_ind_zero);
 
     Edge const* cur_edge;
 
     while (edges_found < ind) {
-        if (cur_ind_day >= airport->edges_from_by_day[day].size()) {
-            assert(cur_ind_zero < airport->edges_from_by_day[0].size());
-            cur_edge = airport->edges_from_by_day[0][cur_ind_zero];
-            iterate_to_next_edge(visited, airport->edges_from_by_day[0], cur_ind_zero);
+        if (cur_ind_day >= mngr->airport->edges_from_by_day[mngr->day].size()) {
+            assert(cur_ind_zero < mngr->airport->edges_from_by_day[0].size());
+            cur_edge = mngr->airport->edges_from_by_day[0][cur_ind_zero];
+            iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[0], cur_ind_zero);
         } else {
-            if (cur_ind_zero >= airport->edges_from_by_day[0].size()) {
-                assert(cur_ind_day < airport->edges_from_by_day[day].size());
-                cur_edge = airport->edges_from_by_day[day][cur_ind_day];
-                iterate_to_next_edge(visited, airport->edges_from_by_day[day], cur_ind_day);
+            if (cur_ind_zero >= mngr->airport->edges_from_by_day[0].size()) {
+                assert(cur_ind_day < mngr->airport->edges_from_by_day[mngr->day].size());
+                cur_edge = mngr->airport->edges_from_by_day[mngr->day][cur_ind_day];
+                iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[mngr->day], cur_ind_day);
             } else {
-                if (airport->edges_from_by_day[0][cur_ind_zero]->cost < airport->edges_from_by_day[day][cur_ind_day]->cost) {
-                    cur_edge = airport->edges_from_by_day[0][cur_ind_zero];
-                    iterate_to_next_edge(visited, airport->edges_from_by_day[0], cur_ind_zero);
+                if (mngr->airport->edges_from_by_day[0][cur_ind_zero]->cost < 
+                    mngr->airport->edges_from_by_day[mngr->day][cur_ind_day]->cost) {
+                    cur_edge = mngr->airport->edges_from_by_day[0][cur_ind_zero];
+                    iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[0], cur_ind_zero);
                 } else {
-                    cur_edge = airport->edges_from_by_day[day][cur_ind_day];
-                    iterate_to_next_edge(visited, airport->edges_from_by_day[day], cur_ind_day);
+                    cur_edge = mngr->airport->edges_from_by_day[mngr->day][cur_ind_day];
+                    iterate_to_next_edge(mngr->visited, mngr->airport->edges_from_by_day[mngr->day], cur_ind_day);
                 }
             }
         }
@@ -71,44 +91,50 @@ Edge const* get_suitable_edge(const vector<bool>& visited, const Airport* airpor
     return cur_edge;
 }
 
-Edge const* get_next_edge(const Assignment* task, const vector<bool>& visited, const Airport* airport, int day) {
-    int suitable_edges_cnt = get_suitable_edges(visited, airport, day, task->max_edge_index);
+Edge const* get_next_edge(GreedyManager* mngr) {
+    int suitable_edges_cnt = get_suitable_edges(mngr);
     if (!suitable_edges_cnt) return nullptr;
 
-    int ind = rand() % min(task->max_edge_index, suitable_edges_cnt) + 1;
-    if (day == task->N) {
+    int ind = rand() % min(mngr->task->max_edge_index, suitable_edges_cnt) + 1;
+    if (mngr->day == mngr->task->N) {
         ind = 1;
     }
-    return get_suitable_edge(visited, airport, day, ind);
+    return get_suitable_edge(mngr, ind);
 } 
 
-vector<const Edge*> get_greedy_path(const Assignment* task, vector<bool> visited, Airport* cur_airport, int cur_day) {
+vector<const Edge*> get_greedy_path(GreedyManager* mngr) {
     vector<const Edge*> path_edges;
-    for (; cur_day <= task->N; ++cur_day) {
-        if (cur_day == task->N) {
-            visited[task->start->zone] = false;
+    for (; mngr->day <= mngr->task->N; ++mngr->day) {
+        if (mngr->day == mngr->task->N) {
+            mngr->visited[mngr->task->start->zone] = false;
         }
 
-        Edge const* next_edge = get_next_edge(task, visited, cur_airport, cur_day);
+        Edge const* next_edge = get_next_edge(mngr);
 
         if (next_edge == nullptr) {
             return {};
         }
 
         path_edges.push_back(next_edge);
-        visited[next_edge->to->zone] = true;
-        cur_airport = next_edge->to;
+        mngr->visited[next_edge->to->zone] = true;
+        mngr->airport = next_edge->to;
     }
     return path_edges;
 }
 
 Solution greedy(const Assignment* task) {
+    GreedyManager mngr(task);
+    /*
     Solution solution{ .task = task };
-    Airport* cur_airport = task->start;
+    Airport* airport = task->start;
     vector<bool> visited(task->N, false);
-    visited[cur_airport->zone] = true;
+    visited[airport->zone] = true;
+    */
+    mngr.solution.sequence = get_greedy_path(&mngr);
 
-    solution.sequence = get_greedy_path(task, move(visited), cur_airport, 1);
+    return mngr.solution;
+}
 
-    return solution;
+Solution greedy_mcts(const Assignment* task) {
+    Solution solution{ .task = task };
 }
