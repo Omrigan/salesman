2f2daa9d3f7e33aeb8606d3cabd4ad66a4679b89
NedoL98 <a.nedol@yandex.ru>
2018-10-26 01:03:47 +0300

    What was that?

--- solutions/greedy_local_search_merged23.cpp
+++
@@ -1,9 +0,0 @@
-#include "../newmain.cpp"
-
-int main(){
-    read_and_solve([](Assignment * task){
-        Solution sol = run_multiple_solutions(greedy, solve_local_search_combined, task);
-        return sol;
-    });
-}
-
