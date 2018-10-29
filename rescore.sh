15104720e80e2eec362d3be92ee64a3bb7c62ddf
Omrigan <omrigann@gmail.com>
2018-10-29 23:14:54 +0300

    FIX

--- rescore.sh
+++ rescore.sh
@@ -2,5 +2,5 @@
 # ./score new_max_edge_index_stochastic
 # ./score new_max_edge_index_stochastic_3opt
 # ./score greedy_local_search_anneal_optimization
-./score greedy_local_search_anneal  
+# ./score greedy_local_search_anneal  
 ./score new_max_edge_index                   
